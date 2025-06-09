4d47e5234bd1e8cec13f92e41b2a10496fab43c3
Derek Hohls <dhohls@creationbreeding.co.za>
2025-06-09 10:43:34 +0200

    Black formatting.

--- protograf/base.py
+++ protograf/base.py
@@ -208,7 +208,9 @@ class BaseCanvas:
         self.grid_marks_stroke_width = self.defaults.get(
             "grid_marks_stroke_width", self.stroke_width
         )
-        self.grid_marks_length = self.defaults.get("grid_marks_length", 0.85)  # 1/3 inch
+        self.grid_marks_length = self.defaults.get(
+            "grid_marks_length", 0.85
+        )  # 1/3 inch
         self.grid_marks_offset = self.defaults.get("grid_marks_offset", 0)
         # ---- line style
         self.line_stroke = self.defaults.get("line_stroke", WIDTH)
@@ -549,8 +551,12 @@ class BaseShape:
         self.grid_marks_stroke_width = self.kw_float(
             kwargs.get("grid_marks_stroke_width", base.grid_marks_stroke_width)
         )
-        self.grid_marks_length = self.kw_float(kwargs.get("grid_marks_length", base.grid_marks_length))
-        self.grid_marks_offset = self.kw_float(kwargs.get("grid_marks_offset", base.grid_marks_offset))
+        self.grid_marks_length = self.kw_float(
+            kwargs.get("grid_marks_length", base.grid_marks_length)
+        )
+        self.grid_marks_offset = self.kw_float(
+            kwargs.get("grid_marks_offset", base.grid_marks_offset)
+        )
         # ---- sizes and positions
         self.row = kwargs.get("row", base.row)
         self.col = self.kw_int(kwargs.get("col", kwargs.get("column", base.col)), "col")
