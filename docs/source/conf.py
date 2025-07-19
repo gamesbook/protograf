ff71ae33ff662e7148c269432648eca991cf7318
Derek Hohls <dhohls@creationbreeding.co.za>
2025-07-19 20:08:47 +0200

    Updates for release 0.2.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2025, Derek R. Hohls'
 author = 'Derek R. Hohls'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.9'
+release = '0.2.10'
 
 
 # ---- General configuration
